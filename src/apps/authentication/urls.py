d22f65f813f4c3362107875d57126c0f8e52f0c0
Lilit <galstyan.lilit.00@mail.com>
2025-04-13 02:19:22 +0400

    Added API for getting profile data

---
+++ src/apps/authentication/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import ProfileAPIView
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+
+urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/profile/', ProfileAPIView.as_view(), name='profile'),
+]
